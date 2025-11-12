a4ea05a06b2d42a8529f9f29372412a22acadeb1
Osborn Eluerkeh <osbornboaheluerkeh@gmail.com>
2025-11-12 22:07:38 +0000

    Initial commit for Vercel deployment

---
+++ src/components/home/module-wise-components/rental/rental-api-manage/ApiRoutes.js
@@ -0,0 +1,7 @@
+const ApiRoutes = {
+  GET_TAX: '/api/tax',
+  CREATE_ORDER: '/api/order',
+  UPDATE_RENTAL: '/api/rental/update',
+};
+
+export default ApiRoutes;
---
+++ src/firebase.js
@@ -0,0 +1,66 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import {
+  getMessaging,
+  getToken,
+  onMessage,
+  isSupported,
+} from "firebase/messaging";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDW6LkQNf1FGBmo5QtD25i7C5BTI-ZVFzo",
+  authDomain: "banca-9ad64.firebaseapp.com",
+  projectId: "banca-9ad64",
+  storageBucket: "banca-9ad64.firebasestorage.app",
+  messagingSenderId: "152904086627",
+  appId: "1:152904086627:web:2e38f4aea82fb97c527456",
+  measurementId: "G-5051EHDSP2",
+};
+const firebaseApp = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
+const messaging = (async () => {
+  try {
+    const isSupportedBrowser = await isSupported();
+    if (isSupportedBrowser) {
+      return getMessaging(firebaseApp);
+    }
+    return null;
+  } catch (err) {
+    return null;
+  }
+})();
+
+export const fetchToken = async (setTokenFound, setFcmToken) => {
+  return getToken(await messaging, {
+    vapidKey:
+      "",
+  })
+    .then((currentToken) => {
+      if (currentToken) {
+        setTokenFound(true);
+        setFcmToken(currentToken);
+
+        // Track the token -> client mapping, by sending to backend server
+        // show on the UI that permission is secured
+      } else {
+        setTokenFound(false);
+        setFcmToken();
+        // shows on the UI that permission is required
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      // catch error while creating client token
+    });
+};
+
+export const onMessageListener = async () =>
+  new Promise((resolve) =>
+    (async () => {
+      const messagingResolve = await messaging;
+      onMessage(messagingResolve, (payload) => {
+        resolve(payload);
+      });
+    })()
+  );
+export const auth = getAuth(firebaseApp);
